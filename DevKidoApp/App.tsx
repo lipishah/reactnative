ccf3a8a83db4cabb6d945df5f071fcfb179bf2da
Lipi Shah <lipi07.shah@gmail.com>
2025-10-19 12:48:08 +0530

    Added webview

--- DevKidoApp/App.tsx
+++ DevKidoApp/App.tsx
@@ -6,11 +6,9 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import { StatusBar, StyleSheet, useColorScheme, View , Text} from 'react-native';
+import { SafeAreaProvider,  useSafeAreaInsets } from 'react-native-safe-area-context';
+import { WebView } from 'react-native-webview';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -27,10 +25,13 @@ function AppContent() {
   const safeAreaInsets = useSafeAreaInsets();
 
   return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
+    <View style={[styles.container, { paddingTop: safeAreaInsets.top }]}>
+      {/* WebView pointed to https://devkido.com */}
+      <WebView
+        source={{ uri: 'https://devkido.com' }}
+        style={{ flex: 1 }}
+        startInLoadingState
+        originWhitelist={['*']}
       />
     </View>
   );
@@ -39,6 +40,7 @@ function AppContent() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 24
   },
 });
 
